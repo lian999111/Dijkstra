4cc52d188cc6a854a5fa5b576e81c4725985e1d2
Larry Liang <lian999111@gmail.com>
2017-09-04 11:48:50 +0800

    Add PriorityQueue constructors along with their helper functions

--- Dijkstra/PriorityQueue.h
+++ Dijkstra/PriorityQueue.h
@@ -1,29 +1,62 @@
 #pragma once
 #include <vector>
 #include <utility>
+#include <limits>
 #include "Graph.h"
 
-using namespace std;
-
-// Priority Queue with custom type of node name
+// Priority Queue with custom type of node names
 template <class T>
 class PriorityQueue
 {
 private:
 	// The priority queue. Every node is stored as a pair with its name as the first
-	// element and its cost to reach as the second.
-	vector<pair<T, int>> pri_queue_;
+	// element and its g-value as the second.
+	std::vector<std::pair<T, int>> pri_queue_;
+
+	static std::vector<std::pair<T, int>> MakePriQueueFromVertices(const std::vector<T>& vertices)
+	{
+		int queue_size = vertices.size();
+		std::vector<std::pair<T, int>> temp_queue;
+		temp_queue.reserve(queue_size);
+
+		for (const auto& vertex : vertices)
+		{
+			// Set the initial g-value to the max of int
+			temp_queue.push_back(std::make_pair(vertex, std::numeric_limits<int>::max()));
+		}
+		return temp_queue;
+	}
+
+	static std::vector<std::pair<T, int>> MakePriQueueFromGraph(const Graph<T>& g)
+	{
+		auto vertices = g.GetVertices();
+
+		return MakePriQueueFromVertices(vertices);
+	}
 public:
 	// Default constructor
 	PriorityQueue();
-	// Constructor with a graph object
-	PriorityQueue(Graph g);
+	// Constructor with a graph object with node names of type T
+	PriorityQueue(const Graph<T>& g);
 	// Constructor with a vector of vertices
-	PriorityQueue(vector<T> vertices);
+	PriorityQueue(const std::vector<T>& vertices);
 
-	~PriorityQueue();
+	//~PriorityQueue();
 };
 
 template <class T>
-PriorityQueue::PriorityQueue()
+PriorityQueue<T>::PriorityQueue()
+	: pri_queue_(std::vector<std::pair<T, int>>())
+{}
+
+template <class T>
+PriorityQueue<T>::PriorityQueue(const Graph<T>& g)
+	: pri_queue_(MakePriQueueFromGraph(g))
+{}
+
+template <class T>
+PriorityQueue<T>::PriorityQueue(const std::vector<T>& vertices)
+	: pri_queue_(MakePriQueueFromVertices(vertices))
+{}
+
 
--- Dijkstra/main.cpp
+++ Dijkstra/main.cpp
@@ -1,8 +1,18 @@
+#include <string>
 #include "Graph.h"
-#include "Dijkstra.h"
+#include "PriorityQueue.h"
 
 
 int main()
 {
+	std::vector<std::string> vertices{ "A", "B", "C", "D", "E" };
+	Graph<std::string> g(vertices, 0.7, 10);
+
+	PriorityQueue<std::string> pq1(vertices);
+	PriorityQueue<std::string> pq2(g);
+	PriorityQueue<std::string> pq3;
+
+
+
 	return 0;
 }
