5065e8334911116512e854f9fee81a2d643b0ff5
Larry Liang <lian999111@gmail.com>
2017-09-08 23:57:27 +0800

    Add data members and a ctor

--- Dijkstra/PathFinder.h
+++ Dijkstra/PathFinder.h
@@ -1,1 +1,33 @@
 #pragma once
+#include <vector>
+#include <map>
+#include <utility>
+#include "Graph.h"
+#include "PriorityQueue.h"
+
+// Shortest path alogorthm with node names of type T
+template <class T>
+class PathFinder
+{
+private:
+	// The start node that this shortest path object is based on
+	T start_node_;
+
+	// The map storing the finished node:
+	// Key:		node name
+	// Value:	a pair containing the parent node and the g-value
+	std::map<T, std::pair<T, int>> closed_;
+
+public:
+	// Default constructor.
+	// Initializes closed as empty
+	PathFinder(T start_node);
+
+};
+
+
+template<class T>
+PathFinder<T>::PathFinder(T start_node)
+	: start_node_(start_node)
+	, closed_(std::map<T, std::pair<T, int>>())
+{}
